4b3247f054f44ffc883d61742ed6865284c419c1
MarwenChouchane <chouchane.marwen@gmail.com>
2022-02-02 14:43:20 +0000

    Parameter@CsvSourceMultipleParameter

--- src/test/java/junit5Test/ParameterizedTest.java
+++ src/test/java/junit5Test/ParameterizedTest.java
@@ -1,9 +1,6 @@
 package junit5Test;
 
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 public class ParameterizedTest {
 
@@ -19,5 +16,23 @@ public class ParameterizedTest {
     void stringValue(String theParam){
         System.out.println("theParam = " + theParam);
     }
+
+    @org.junit.jupiter.params.ParameterizedTest (name = "Run : {index} - value = {arguments}")
+    @CsvSource (value = {"Steve,Roger" , "Captain,Marvel" , "Bucky,Barnes"})
+    void csvSource_StringString(String param1 , String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @org.junit.jupiter.params.ParameterizedTest (name = "Run : {index} - value = {arguments}")
+    @CsvSource(value = {"Steve,32,true", "Captain,55,false","Bucky,5,true"})
+    void csvSource_StringIntBoolean(String param1 , int param2 , boolean param3){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2 + ", param3 = " + param3);
+    }
+    @org.junit.jupiter.params.ParameterizedTest (name = "Run : {index} - value = {arguments}")
+    @CsvSource(value = {"Captain America , 'Steve,Roger'", "Winter soldier ,'Bucky,Barnes'"})
+    void csvSource_StringWithComa(String param1 , String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
 
 }
