5fb981f09cfcae6b184cc8a622a06e11d15625ba
MarwenChouchane <chouchane.marwen@gmail.com>
2022-02-10 11:22:34 +0000

    CsvFileSource

--- src/test/java/junit5Test/ParameterizedTest.java
+++ src/test/java/junit5Test/ParameterizedTest.java
@@ -2,6 +2,8 @@ package junit5Test;
 
 import org.junit.jupiter.params.provider.*;
 
+import java.util.List;
+
 public class ParameterizedTest {
 
     @org.junit.jupiter.params.ParameterizedTest(name = "Run : {index} - value = {arguments}")
@@ -34,5 +36,26 @@ public class ParameterizedTest {
         System.out.println("param1 = " + param1 + ", param2 = " + param2);
     }
 
+    @org.junit.jupiter.params.ParameterizedTest
+    @CsvFileSource(files = {"src/test/resources/params/shoppingList.csv" , "src/test/resources/params/shoppingList2.csv"}, numLinesToSkip = 1)
+    void csvFileSource_StringDoubleIntStringString(String name, Double price, int quantity, String unit, String provider){
+        System.out.println("name = " + name + ", price = " + price + ", quantity = " + quantity + ", unit = " + unit + ", provider = " + provider);
+    }
+
+    @org.junit.jupiter.params.ParameterizedTest
+    @CsvFileSource(files = {"src/test/resources/params/shoppingList3.csv"}, numLinesToSkip = 1, delimiterString = "++")
+    void csvFileSource_StringDoubleIntStringStringPersonelazedDelemiter(String name, Double price, int quantity, String unit, String provider){
+        System.out.println("name = " + name + ", price = " + price + ", quantity = " + quantity + ", unit = " + unit + ", provider = " + provider);
+    }
+
+    @org.junit.jupiter.params.ParameterizedTest
+    @MethodSource(value = "sourceString")
+    void methodeSource_String(String param1){
+
+    }
+
+    List<String> sourceString{
+
+    }
 
 }
